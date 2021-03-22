5139d3e652a720532801d543ddf30878c6b15b30
Nick van Raaij <dagelijksnick@gmail.com>
2021-03-22 16:33:43 +0100

    week 1 en 2 done

--- js/feedbackWidget.js
+++ js/feedbackWidget.js
@@ -5,21 +5,55 @@ class FeedbackWidget{
     get elementId() { //getter, set keyword voor setter methode
         return this._elementId;
     }
-    show(elementId, message, type) {
-        const x = document.getElementById(elementId);
+    show(message, type) {
+        const x = document.getElementById(this.elementId);
         x.style.display = "block";
-        $(`#${elementId} p`).text(message);
+        $(`#${this.elementId}`).text(message);
 
         if (type === "success") {
-            $(`#${elementId}`).addClass("alert-success");
+            $(`#${this.elementId}`).addClass("alert-success");
         } else {
-            $(`#${elementId}`).addClass("alert-danger");
+            $(`#${this.elementId}`).addClass("alert-danger");
         }
+
+        this.log({ message: message, type: type});
     }
 
-    hide(elementId) {
-        const x = document.getElementById(elementId);
+    hide() {
+        const x = document.getElementById(this.elementId);
+
+        $(`#${this.elementId}`).removeClass("alert-danger");
+        $(`#${this.elementId}`).removeClass("alert-success");
+
         x.style.display = "none";
+    }
+
+    log(message) {
+        if (localStorage.getItem('feedback_widget') === null) {
+            localStorage.setItem('feedback_widget', JSON.stringify([message]))
+        }
+        const storageArray = JSON.parse(localStorage.getItem('feedback_widget'));
+        storageArray[storageArray.length] = message;
+        localStorage.setItem('feedback_widget', JSON.stringify(storageArray));
+    }
+
+    removeLog() {
+        localStorage.removeItem('feedback_widget');
+    }
+
+    history() {
+        const log = JSON.parse(localStorage.getItem('feedback_widget'));
+        let string = '';
+
+        log.forEach(item => {
+            if (item === log[log.length - 1]) {
+                string += `<${item.type}> ${item.message}`;
+            } else {
+                string += `<${item.type}> ${item.message} <\n>`;
+            }
+        })
+
+        this.show(string);
     }
 }
 
---
+++ js/game.js
@@ -0,0 +1,90 @@
+const Game = (function(url){
+
+    //Configuratie en state waarden
+    let configMap = {
+        apiUrl: url
+    }
+
+    console.log('hallo, vanuit een module');
+    // Private function init
+    const privateInit = function(callback){
+        console.log(configMap.apiUrl);
+
+        callback();
+    }
+
+    // Waarde/object geretourneerd aan de outer scope
+    return {
+        init: privateInit,
+    }
+
+})('/api/url')
+
+
+Game.Reversi = (function() {
+    let _configMap = {};
+
+    const privateInit = function(){
+        console.log('hallo, vanuit module Reversi');
+    }
+
+    return {
+        init: privateInit
+    }
+})()
+
+Game.Data = (function() {
+    let configMap = {
+        mock: [
+            {
+                url: api/Spel/Beurt,
+                data: 0
+            }
+        ]
+    }
+
+    let stateMap = { environment: 'development' };
+
+    const get = function(url){
+
+        // const getMockData = function(url){
+        //     //filter mock data, configMap.mock ... oei oei, moeilijk moeilijk :-)
+        //     const mockData =
+        //
+        //     return new Promise((resolve, reject) => {
+        //         resolve(mockData);
+        //     });
+        // }
+
+        return getMockData(url);
+    }
+
+    const privateInit = function(environment){
+        stateMap.environment = environment;
+
+        if (stateMap.environment === 'production') {
+
+        } else if (stateMap.environment === 'development') {
+            get(configMap.mock.url);
+        } else {
+            new Error('Oeps er is geen environment')
+        }
+    }
+
+    return {
+        init: privateInit,
+    }
+})()
+
+Game.Model = (function() {
+    let configMap = {};
+
+    const privateInit = function(){
+    }
+
+    return {
+        init: privateInit,
+    }
+})()
+
+
