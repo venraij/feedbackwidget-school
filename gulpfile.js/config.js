05c87b7b13dbe260de54cb72137ca8581b239bea
Nick van Raaij <dagelijksnick@gmail.com>
2021-03-22 22:42:14 +0100

    added npm and gulp

---
+++ gulpfile.js/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    localServerProjectPath : 'C:/Users/nickv/source/repos/ReversiMvcApp/ReversiMvcApp/wwwroot/js',
+    files: {
+        js: [
+            'js/**/*.js',
+            'js/*.js'
+        ]
+    },
+    voornaam: 'Nick'
+};
---
+++ gulpfile.js/index.js
@@ -0,0 +1,15 @@
+const config = require('./config');
+const js = require('./tasks/js').js(config.localServerProjectPath);
+
+js.displayName = 'js';
+
+
+const hello = function (done) {
+    console.log(`Groeten van ${config.voornaam}!`)
+    done();
+}
+
+
+exports.default = hello;
+
+exports.js = js;
